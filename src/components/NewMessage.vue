1c76f69463d3b2ae61141909c56712a59ddcdfb3
Haizhepi <yunzheliu1998@gmail.com>
2020-10-13 21:21:08 -0700

    Real Time Chatting

---
+++ src/components/NewMessage.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="new-message">
+        <form @submit.prevent="addMessage">
+            <label for="new-message">New Message (enter to add): </label>
+            <input type="text" name="new-message" v-model="newMessage">
+            <p class="red-text center" v-if="feedback">{{ feedback }}</p>
+        </form>
+    </div>
+</template>
+
+<script>
+import db from '@/firebase/init'
+export default {
+    name: "NewMessage",
+    props: ['name'],
+    data() {
+        return {
+            newMessage: null,
+            feedback: null
+        }
+    },
+    methods: {
+        addMessage() {
+            if (this.newMessage) {
+                db.collection('messages').add({
+                    content: this.newMessage,
+                    name: this.name,
+                    timestamp: Date.now()
+                }).catch(err => {
+                    console.log(err);
+                })
+                this.newMessage = null;
+                this.feedback = null;
+            console.log(this.newMessage, this.name, Date.now());
+            }
+            else {
+                this.feedback = "You must enter a message"
+            }
+        }
+    }
+}
+</script>
